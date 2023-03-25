94b632ad96786e3f9faf501e70b7e3db47de7bcb
Joshua Oluwasola <theofficialtjosh@gmail.com>
2023-03-26 00:14:22 +0100

    Allowed host

--- dynamicportfolio/settings.py
+++ dynamicportfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['joshuas-portfolio.onrender.com']
 
 
 # Application definition
