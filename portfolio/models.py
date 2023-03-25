6217e49677890d21dd6e7884dfa21a0b68e98d2e
Joshua Oluwasola <theofficialtjosh@gmail.com>
2023-03-26 00:05:29 +0100

    Deploying to render

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    img_url = models.URLField(max_length=2000)
+    img_url = models.CharField(max_length=4000)
     title = models.CharField(max_length=50)
     text = models.CharField(max_length=255)
     link = models.URLField(max_length=1000)
 
 
 class Skill(models.Model):
-    img_url = models.URLField(max_length=2000)
+    img_url = models.CharField(max_length=4000)
     text = models.CharField(max_length=50)
 
 class About(models.Model):
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -7,3 +10,6 @@ urlpatterns = [
     path('contact', views.contact, name='contact'),
     path('project', views.project, name='project')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
